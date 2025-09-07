3f925eb41befec6332b1a812f0f1202bb9faccc5
Diego Cuellar <155080634+YeyoDC@users.noreply.github.com>
2025-09-07 20:48:18 -0300

    Update 2025_06_30_152216_create_chore_occurrences_table.php

--- database/migrations/2025_06_30_152216_create_chore_occurrences_table.php
+++ database/migrations/2025_06_30_152216_create_chore_occurrences_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chore_recurrences');
+        Schema::dropIfExists('chore_occurrences');
     }
 };
