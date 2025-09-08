466fa2bb710237b23f5c0aa40fed1f7687685e83
Diego Cuellar <155080634+YeyoDC@users.noreply.github.com>
2025-09-07 21:20:41 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/debug', function () {
+    Log::error('Testing Laravel logging on Heroku.');
+    return 'Check logs!';
+});
+
 
 Route::get('/', function () {
     return view('auth/login');
