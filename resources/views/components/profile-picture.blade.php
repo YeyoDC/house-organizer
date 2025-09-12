97de7c242de87d9300afdacfbdd6e16b9eda4e96
Diego Cuellar <155080634+YeyoDC@users.noreply.github.com>
2025-09-11 21:04:40 -0300

    Update profile-picture.blade.php

--- resources/views/components/profile-picture.blade.php
+++ resources/views/components/profile-picture.blade.php
@@ -1,3 +1,4 @@
+@php use Illuminate\Support\Facades\Storage; @endphp
 @props([
     'user' => null,
     'size' => '8',
@@ -7,8 +8,16 @@
 @php
     $user = $user ?? auth()->user();
     $profilePicture = $user->profile_picture;
-    $src = $profilePicture ? asset($profilePicture) : asset('storage/default-avatar.png');
+
+    if ($profilePicture) {
+        // Use Storage::url so it works with local/public or S3 automatically
+        $src = Storage::url($profilePicture);
+    } else {
+        // Fallback default avatar (kept in public/storage for both envs)
+        $src = asset('storage/default-avatar.png');
+    }
 @endphp
+
 
 <img
     src="{{ $src }}"
