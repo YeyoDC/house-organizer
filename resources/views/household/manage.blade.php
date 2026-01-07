16b8bdd182ed05c501fa909376ca76a1dce85bff
Diego Cuellar <155080634+YeyoDC@users.noreply.github.com>
2026-01-07 08:30:57 -0400

    Update manage.blade.php

--- resources/views/household/manage.blade.php
+++ resources/views/household/manage.blade.php
@@ -5,11 +5,10 @@
                 @include('household.partials.household_form')
             @endif
             @if($household)
-                {{ dd($household->name)}}
                 {{-- if they have created a household already --}}
                 <div class="flex items-center justify-center space-x-4 mb-4">
                     <h1 class="font-bold text-xl">{{ $household->name }}</h1>
-                    <x-household-picture :household="$household" size="9" class="shadow-md" />
+{{--                    <x-household-picture :household="$household" size="9" class="shadow-md" />--}}
 
                 </div>
 
