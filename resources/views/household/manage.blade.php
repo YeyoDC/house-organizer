6e4e7ce2e6bb98df5e9b89c64e20884927df9c6c
Diego Cuellar <155080634+YeyoDC@users.noreply.github.com>
2026-01-09 09:25:06 -0400

    Update manage.blade.php

--- resources/views/household/manage.blade.php
+++ resources/views/household/manage.blade.php
@@ -8,7 +8,7 @@
                 {{-- if they have created a household already --}}
                 <div class="flex items-center justify-center space-x-4 mb-4">
                     <h1 class="font-bold text-xl">{{ $household->name }}</h1>
-                    <x-household-picture :household="$household" size="9" class="shadow-md" />
+                    <x-household-picture :household="$household" size="6" class="shadow-md" />
 
                 </div>
 
